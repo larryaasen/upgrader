879d54b63fa0283f839f57ac0ce9f2c715b09af5
Ulisses Hen <ulisses@hens.com.br>
2022-11-12 22:48:43 -0300

    add test style for cupertino dialog style

--- test/upgrade_style_test.dart
+++ test/upgrade_style_test.dart
@@ -115,6 +115,99 @@ void main() {
     expect(upgrader.textStyles.prompt!.fontFamily, "Roboto");
   }, skip: false);
 
+  testWidgets(
+      'test upgrader with text styles applied in IOS and UpgradeDialogStyle.cupertino',
+      (WidgetTester tester) async {
+    final client = MockITunesSearchClient.setupMockClient();
+    final upgrader = _getUpgrader(client, TargetPlatform.iOS);
+
+//Variables test
+    expect(upgrader.textStyles.title, isNotNull);
+    expect(upgrader.textStyles.message, isNotNull);
+    expect(upgrader.textStyles.titleReleaseNotes, isNotNull);
+    expect(upgrader.textStyles.bodyReleaseNotes, isNotNull);
+    expect(upgrader.textStyles.prompt, isNotNull);
+
+    expect(upgrader.textStyles.title!.backgroundColor, Colors.amber);
+    expect(upgrader.textStyles.message!.fontSize, 16);
+
+    expect(upgrader.textStyles.titleReleaseNotes!.fontSize, 18);
+    expect(upgrader.textStyles.titleReleaseNotes!.fontWeight, FontWeight.w900);
+
+    expect(upgrader.textStyles.bodyReleaseNotes!.color, Colors.black);
+    expect(upgrader.textStyles.bodyReleaseNotes!.fontWeight, FontWeight.w700);
+
+    expect(upgrader.textStyles.prompt!.fontFamily, "Roboto");
+
+// Visibility Alert tests
+    upgrader.dialogStyle = UpgradeDialogStyle.cupertino;
+    upgrader.installPackageInfo(
+        packageInfo: PackageInfo(
+            appName: 'Upgrader',
+            packageName: 'com.larryaasen.upgrader',
+            version: '0.9.9',
+            buildNumber: '400'));
+
+    await upgrader.initialize();
+
+    expect(upgrader.isUpdateAvailable(), true);
+
+    expect(upgrader.isTooSoon(), false);
+
+    await tester.pumpWidget(MyWidgetTest(upgrader: upgrader));
+
+    expect(find.text('Upgrader test'), findsOneWidget);
+    expect(find.text('Upgrading'), findsOneWidget);
+
+    // Pump the UI so the upgrader can display its dialog
+    await tester.pumpAndSettle();
+    await tester.pumpAndSettle();
+
+    expect(upgrader.isTooSoon(), true);
+
+    final titleStyle = _findTextStyleByText(
+      tester,
+      upgrader.messages.title,
+    );
+
+    expect(titleStyle, isNotNull);
+    expect(titleStyle!.backgroundColor, Colors.amber);
+
+    final messageStyle = _findTextStyleByText(
+      tester,
+      upgrader.message(),
+    );
+
+    expect(messageStyle, isNotNull);
+    expect(messageStyle!.fontSize, 16);
+
+    final promptStyle = _findTextStyleByText(
+      tester,
+      upgrader.messages.prompt,
+    );
+
+    expect(promptStyle, isNotNull);
+    expect(promptStyle!.fontFamily, "Roboto");
+
+    final titleReleaseStyle = _findTextStyleByText(
+      tester,
+      upgrader.messages.releaseNotes,
+    );
+
+    expect(titleReleaseStyle, isNotNull);
+    expect(titleReleaseStyle!.fontSize, 18);
+    expect(titleReleaseStyle.fontWeight, FontWeight.w900);
+
+    final bodyReleaseStyle = _findTextStyleByText(
+      tester,
+      upgrader.releaseNotes!,
+    );
+
+    expect(bodyReleaseStyle, isNotNull);
+    expect(bodyReleaseStyle!.color, Colors.black);
+    expect(bodyReleaseStyle.fontWeight, FontWeight.w700);
+  }, skip: false);
+
   testWidgets('test upgrader with text styles applied in IOS',
       (WidgetTester tester) async {
     final client = MockITunesSearchClient.setupMockClient();
