5e7a36c0f6648e222932dac5cd37e7d8074e8b80
amarell <mujagicamar@gmail.com>
2024-04-16 17:37:29 +0200

    Re-added empty lines

--- lib/src/upgrade_messages.dart
+++ lib/src/upgrade_messages.dart
@@ -45,8 +45,10 @@ enum UpgraderMessage {
 ///   @override
 ///   String get buttonTitleIgnore => 'My Ignore';
 /// }
+///
 /// UpgradeAlert(upgrader: Upgrader(messages: MyUpgraderMessages());
 /// ```
+///
 class UpgraderMessages {
   /// The primary language subtag for the locale, which defaults to the
   /// system-reported default locale of the device.
